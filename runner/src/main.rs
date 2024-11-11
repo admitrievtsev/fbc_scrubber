eb1650c87b3d3e3f5029f7c0478cf9b3d9adcc25
Aleksei <dmt.alex2005@gmail.com>
2024-11-11 14:20:15 +0300

    dev: dev-only project re-arrange
    Signed-off-by: Aleksei <dmt.alex2005@gmail.com>

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,26 +1,32 @@
-extern crate fbc_scrubber;
 extern crate chunkfs;
+extern crate fbc_scrubber;
 
-use fbc_scrubber::{FBCMap, FBCScrubber};
-use std::io;
-use chunkfs::FileSystem;
-use std::collections::HashMap;
-use chunkfs::hashers::Sha256Hasher;
 use chunkfs::chunkers::SuperChunker;
+use chunkfs::hashers::Sha256Hasher;
+use chunkfs::FileSystem;
+use fbc_scrubber::storage::FBCMap;
+use fbc_scrubber::FBCScrubber;
+use std::collections::HashMap;
+use std::io;
 
-fn main() {
+fn main() -> io::Result<()> {
     /*
     let mut analyser = Analyser::default();
     analyser.deduplicate("test_files_input/lowinput.txt", "lowout.txt");
     fs::remove_file("lowout.txt").expect("File lowout.txt not exists in current directory");
     */
-    let mut fs = FileSystem::new(HashMap::default(), Box::new(FBCMap::new()), Box::new(FBCScrubber::new()), Sha256Hasher::default());
+    let mut fs = FileSystem::new_with_scrubber(
+        HashMap::default(),
+        Box::new(FBCMap::new()),
+        Box::new(FBCScrubber::new()),
+        Sha256Hasher::default(),
+    );
     let mut handle = fs.create_file("file".to_string(), SuperChunker::new(), true)?;
     let data = vec![10; 1024 * 1024];
     fs.write_to_file(&mut handle, &data)?;
     fs.close_file(handle)?;
 
-    let res = fs.scrub().unwrap();
+    let res = fs.scrub()?;
     println!("{res:?}");
 
     let mut handle = fs.open_file("file", SuperChunker::new())?;
